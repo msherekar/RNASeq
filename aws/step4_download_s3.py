9ee430b42eb46c35a6a73fa5045f80a488f0a602
Mukul Sherekar <mukulsherekar@gmail.com>
2025-02-15 10:50:48 -0500

    logging

--- aws/step4_download_s3.py
+++ aws/step4_download_s3.py
@@ -1,12 +1,16 @@
 # step4_download_s3.py
 import boto3
 import config
+import logging
 
 def download_results():
     s3 = boto3.client("s3")
-    print("📥 Downloading results from S3...")
-    s3.download_file(config.S3_BUCKET, "results/gene_counts_matrix.csv", "gene_counts_matrix.csv")
-    print("✅ Download complete.")
+    logging.info("📥 Downloading results from S3...")
+    try:
+        s3.download_file(config.S3_BUCKET, "results/gene_counts_matrix.csv", "gene_counts_matrix.csv")
+        logging.info("✅ Download complete.")
+    except Exception as e:
+        logging.error("❌ Error downloading results from S3: %s", e)
 
 if __name__ == "__main__":
     download_results()
